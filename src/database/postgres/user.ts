8895efc66e23e40aae7ed2eb311dbb593e64ed37
XiovV <xiovv@tutanota.com>
2023-02-17 18:28:56 +0100

    UserRepository and methods for inserting the user and finding it by username

--- src/database/postgres/user.ts
+++ src/database/postgres/user.ts
@@ -3,10 +3,29 @@ import { BaseUser, User } from "../../models/user";
 import UserService from "../user.interface";
 
 export default class UserRepository implements UserService {
-    constructor(private pool: Pool) {} 
+    constructor(private pool: Pool) { }
 
-    insertUser(user: BaseUser): User {
-        throw new Error("Method not implemented.");
+    async insertUser(user: BaseUser): Promise<[number, unknown]> {
+        try {
+            const result = await this.pool.query(`INSERT INTO "user" (username, email, password) VALUES ($1, $2, $3) RETURNING id`, [user.username, user.email, user.password])
+
+            const id = result.rows[0].id
+            return [id, null]
+        } catch (error) {
+            return [0, null]
+        }
     }
-    
+
+    async findUserByUsername(username: string): Promise<User> {
+            const result = await this.pool.query(`SELECT * FROM "user" WHERE username = $1`, [username])
+
+            const user: User = result.rows[0]
+
+            if (typeof user === 'undefined') {
+                throw new Error('user could not be found')
+            }
+
+            return user 
+    }
+
 }
