6051bec12b692c762d112c498aef338a789cc832
XiovV <xiovv@tutanota.com>
2023-02-17 18:28:27 +0100

    interface for the UserService

--- src/database/user.interface.ts
+++ src/database/user.interface.ts
@@ -1,5 +1,6 @@
 import { BaseUser, User } from "../models/user";
 
 export default interface UserService {
-    insertUser(user: BaseUser): User
+    insertUser(user: BaseUser): Promise<[number, unknown]>
+    findUserByUsername(username: string): Promise<User>
 }
