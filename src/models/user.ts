a193db878b36030b9fac7048c988e24a23c6b093
XiovV <xiovv@tutanota.com>
2023-02-17 18:27:11 +0100

    interfaces for the User model and validation schemas

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,26 @@
+import Joi, { string } from "joi"
+
 export interface BaseUser {
     username: string,
     email: string,
     password: string,
-    active: boolean,
 }
 
 export interface User extends BaseUser {
-    id: number
+    id: number,
+    mfa_secret: string,
+    role: number,
+    recovery: string[],
+    active: boolean,
 }
+
+export const baseUserValidationSchema = Joi.object({
+    username: Joi.string().min(2).max(50).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required()
+})
+
+export const loginUserValidationSchema = Joi.object({
+    username: Joi.string().min(2).max(50).required(),
+    password: Joi.string().min(8).required()
+})
