c8ca1e7623693e9df4cc2f06e1da789a25b82133
XiovV <xiovv@tutanota.com>
2023-02-17 18:27:56 +0100

    setting up dependencies and starting up the server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import * as dotenv from 'dotenv'
+import * as env from 'env-var'
 import newPool from './database/postgres/pool'
 import UserRepository from './database/postgres/user'
 import UsersRouter from './routes/users.router'
 import Server from './server/server'
 dotenv.config()
 
-const pool = newPool(process.env.POSTGRES_HOST, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, process.env.POSTGRES_DATABASE)
+const PORT: number = env.get('PORT').default('5000').asPortNumber()
+const POSTGRES_HOST: string = env.get('POSTGRES_HOST').required().asString()
+const POSTGRES_USERNAME: string = env.get('POSTGRES_USERNAME').required().asString()
+const POSTGRES_PASSWORD: string = env.get('POSTGRES_PASSWORD').required().asString()
+const POSTGRES_DATABASE: string = env.get('POSTGRES_DATABASE').required().asString()
+
+
+const pool = newPool(POSTGRES_HOST, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_DATABASE)
 const userRepository = new UserRepository(pool)
 
 const usersRouter = new UsersRouter(userRepository)
 
 
-const server = new Server(parseInt(process.env.PORT || "5000"), usersRouter)
+const server = new Server(PORT, usersRouter)
 server.start()
