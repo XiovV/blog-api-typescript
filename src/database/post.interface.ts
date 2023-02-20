c4f5ef70c9454cedfc1997c934bdc69f2a048e03
XiovV <xiovv@tutanota.com>
2023-02-20 19:19:30 +0100

    added getPostsByUserId to PostService interface

--- src/database/post.interface.ts
+++ src/database/post.interface.ts
@@ -3,4 +3,5 @@ import { BasePost, Post } from "../models/post";
 export default interface PostService {
     insertPost(post: BasePost): Promise<Post>
     getPostById(postId: number): Post
+    getPostsByUserId(userId: number): Promise<Post[]> 
 }
